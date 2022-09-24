50373c57c018dfcdbfa263ddb4ca320499c2236a
i-Vini <vncnsc1212@gmail.com>
2022-09-23 22:55:17 -0300

    ASP net Core + Migration

---
+++ Data/AplicacaoContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Aplicacao.Models;
+
+namespace Aplicacao.Data
+{
+    public class AplicacaoContext : DbContext
+    {
+        public AplicacaoContext (DbContextOptions<AplicacaoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+
+
+
+    }
+}
---
+++ Models/Enum/SaleStatus.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Aplicacao.Models.Enum
+{
+    public enum SaleStatus : int
+    {
+        Pending = 0,
+        Billed = 1,
+        Canceled = 2
+    }
+}
